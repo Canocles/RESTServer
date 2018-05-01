3fcdeca2fc392cfa51503ea7a44caa50a6168064
Ramsés Martínez Martínez <rmm118@alu.ua.es>
2018-05-01 04:07:00 +0200

    Configuración de variables de entorno para MongoDB y Heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 // Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// BBDD
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost/cafe';
+} else {
+    urlDB = 'mongodb://admin:admin@ds261929.mlab.com:61929/heroku_gzfvsbbp';
+}
+
+process.env.URLDB = urlDB;
