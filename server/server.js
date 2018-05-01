f5dccf10adc02ad36df7701d4d1b5645ade7fbf2
Ramsés Martínez Martínez <rmm118@alu.ua.es>
2018-05-01 17:48:29 +0200

    Añadida página estática para logearse mediante Google

--- server/server.js
+++ server/server.js
@@ -3,18 +3,22 @@ require('./config/config')
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// Habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')))
+
 // Configuración de rutas
 app.use(require('./routes/routes'))
 
 mongoose.connect(process.env.URLDB, (err, res) => {
     if (err) throw err;
-    console.log('Base de datos ONLINE');
+    console.log('Base de datos ONLINE')
 })
 
 app.listen(process.env.PORT, () => {
