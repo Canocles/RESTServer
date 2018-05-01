d3dbd232b7d504f9642ccba88a48b53478bac28d
Ramsés Martínez Martínez <rmm118@alu.ua.es>
2018-05-01 04:33:23 +0200

    Añadida variable de entorno MONGODB

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://admin:admin@ds261929.mlab.com:61929/heroku_gzfvsbbp';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
