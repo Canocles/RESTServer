9e533bd5e7961e10902013844238f216957a9721
Ramsés Martínez Martínez <rmm118@alu.ua.es>
2018-05-04 01:30:06 +0200

    Añadido mostrado de imágenes con token

--- server/routes/imagenes.js
+++ server/routes/imagenes.js
@@ -2,17 +2,22 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+const { verificarToken } = require('../middlewares/auth');
+
 const app = express();
 
-app.get('/imagen/:tipo/:img', (req, res) => {
+app.get('/imagen/:tipo/:img', verificarToken, (req, res) => {
     let tipo = req.params.tipo;
     let img = req.params.img;
 
-    let pathImg = `./uploads/${tipo}/${img}`;
+    let pathImg = path.resolve(__dirname, `../../uploads/${tipo}/${img}`);
 
-    let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg');
-
-    res.sendFile(noImagePath);
+    if (fs.existsSync(pathImg)) {
+        res.sendFile(pathImg);
+    } else {
+        let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg');
+        res.sendFile(noImagePath);
+    }
 });
 
 module.exports = app;
